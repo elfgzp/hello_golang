c6e2be0605e9998ef508584a5d9406e638e50c79
Gzp_ <741424975@qq.com>
2018-12-19 15:57:23 +0800

    [ADD] add import.go

---
+++ import.go
@@ -0,0 +1,17 @@
+package hello_golang
+
+// import 语句类似python中的import
+// 这里不同的是import不能导入源码中没有用到的包，否则Go语言编译器会直接报错
+/* import 第一种方式：
+import "a"
+import "b"
+import "c"
+ */
+/* import 第二种方式：
+import (
+	"a"
+    "b"
+	"c"
+)
+ */
+
