3cdf5c54b0db2c86d1a816eac19140e9d7cb8b51
Gzp_ <741424975@qq.com>
2018-12-19 15:30:18 +0800

    [ADD] add grammar.go

---
+++ grammar.go
@@ -0,0 +1,33 @@
+// 程序所属包, 必须在首行
+package main
+
+// 依赖包
+import "fmt"
+
+// 单行注释
+
+/*
+多行注释
+ */
+
+const HELLOGO string = "hello go" // 常量定义
+var helloGo string = "hello go"   // 变量定义
+
+type goInt int // 一般类型声明
+
+type GoStruct struct { // 声明结构体
+}
+
+type IGo interface {  // 声明接口
+}
+
+
+func goFunc()  {  // 函数定义
+	fmt.Println("goFunc")
+}
+
+func main() {
+	fmt.Println(HELLOGO)
+	fmt.Println(helloGo)
+	goFunc()
+}
