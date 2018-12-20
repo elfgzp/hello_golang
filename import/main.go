e9b03709e2eb35858da03bf7f1ce5f4ea4977d91
Gzp_ <741424975@qq.com>
2018-12-20 17:00:38 +0800

    [ADD] import . _

--- import/main.go
+++ import/main.go
@@ -2,8 +2,8 @@ package _import
 
 import (
 	"fmt"
-	"hello_golang/import/p1"
-	"hello_golang/import/p2"
+	pack1 "hello_golang/import/p1"
+	_ "hello_golang/import/p2"
 )
 
 // import 语句类似python中的import
@@ -27,12 +27,16 @@ import (
 // 所有包导入完成后，才会执行main中的初始化，然后执行main中的init(),最后在执行main()
 // 如果一个包被导入多次，实际只会导入一次
 
+// import 别名
+// 将import包可以起名为别的名字
+// 点(.)标示导入包后，调用该包的函数可以不用包名前缀
+// 下划线(_) 导入后，导入的包只执行init()
+
 func init() {
 	fmt.Println("import run main init()")
 }
 
 func ImportPrint() {
 	fmt.Println("import run main main()")
-	fmt.Println(p1.P1)
-	fmt.Println(p2.P2)
+	fmt.Println(pack1.P1)
 }
