6de43678c6c133a9c527d7ee0cdca50f9fe54f3c
Gzp_ <741424975@qq.com>
2018-12-22 11:58:16 +0800

    [MOD] mod operator

--- operator/main.go
+++ operator/main.go
@@ -11,4 +11,23 @@ func main() {
 	b --
 	fmt.Println(a)
 	fmt.Println(b)
+
+	fmt.Println(a >= b)
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+
+	fmt.Println(a != b && a >= b)
+	fmt.Println(a == b || a >= b)
+
+	c := byte(0)
+	d := byte(1)
+
+	fmt.Println(c & d)
+	fmt.Println(c | d)
+	fmt.Println(c ^ d)
+	fmt.Println(d>>1)
+	fmt.Println(d<<1)
+
+	a += b
+	fmt.Println(a)
 }
