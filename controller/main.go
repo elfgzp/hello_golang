32553a5d8cc4971da360948e951ec0a22a43740f
Gzp_ <741424975@qq.com>
2018-12-24 14:44:07 +0800

    [ADD] add controller

--- controller/main.go
+++ controller/main.go
@@ -1,1 +1,34 @@
-package controller
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("loop", i)
+	}
+
+	a := []string{"a", "b", "c"}
+	for key, value := range a {
+		fmt.Println(key, value)
+	}
+
+	goto One
+	fmt.Println("skip")
+One:
+	fmt.Println("This is One")
+
+	for {
+		fmt.Println("loop and break")
+		break
+	}
+
+	for i := 1; i < 3; i++ {
+		if i > 1 {
+			continue
+		}
+		fmt.Println("loop continue")
+	}
+
+}
