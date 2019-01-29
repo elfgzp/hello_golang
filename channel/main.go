6cc951fd6e075a608d37bb6d2531c86e89b55e64
Gzp_ <741424975@qq.com>
2019-01-29 22:08:00 +0800

    [MOD] modify channel

--- channel/main.go
+++ channel/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func makeCakeAndSend(cs chan string, count int) {
+	for i := 1; i <= count; i++ {
+		cakeName := "Strawberry Cake " + strconv.Itoa(i)
+		cs <- cakeName //send a strawberry cake
+	}
+}
+
+func receiveCakeAndPack(cs chan string) {
+	for s := range cs {
+		fmt.Println("Packing received cake: ", s)
+	}
+}
 
 func main() {
-	c := make(chan int)
-	defer close(c)
-	go func() { c <- 3 + 4 }()
-	i := <-c
-	fmt.Println(i)
+	cs := make(chan string)
+	go makeCakeAndSend(cs, 5)
+	go receiveCakeAndPack(cs)
+
+	//sleep for a while so that the program doesn’t exit immediately
+	time.Sleep(3 * 1e9)
 }
